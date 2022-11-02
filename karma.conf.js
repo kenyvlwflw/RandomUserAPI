ee7f19f352cddde19544ee8a79fe35c3781ebabc
keny <kenytonin@gmail.com>
2022-11-02 19:59:06 -0300

    final

--- karma.conf.js
+++ karma.conf.js
@@ -1,28 +1,17 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('@chiragrupani/karma-chromium-edge-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      jasmine: {},
+      clearContext: false 
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/ngv'),
@@ -36,9 +25,9 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    autoWatch: false,
+    browsers: ['Edge'],
+    singleRun: true,
+    restartOnFileChange: false,
   });
 };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { HomePageModule } from './components/home/home.module';
+import { DialogModule } from './components/dialog/dialog.module';
+
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    IonicModule.forRoot(), 
+    IonicStorageModule.forRoot(),
+    BrowserAnimationsModule,
+    HomePageModule,
+    DialogModule
+  ],
+  providers: [{ 
+    provide: {
+      RouteReuseStrategy,
+      HttpClient,
+    },
+    useClass: IonicRouteStrategy,
+  }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
